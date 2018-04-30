79a42741e586749a99a50b37f00ea0470b405c48
Oleh <radyukolegg@gmail.com>
2018-04-30 16:01:37 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,67 @@
+/* alert('Hello'); */
+// document.write('<h1>World</h1>');
+// console.log('Hello World');
+
+// const carName = 'Tesla Model X';
+// const purchaseCarPrice = 100000 + 10000;
+// const isAvailable = true;
+
+// document.write(carName);
+// document.write('<br>');
+// document.write(carPrice);
+// document.write('<br>');
+// document.write(isAvailable);
+// document.write('<br>');
+
+// const saleCarPrice = carPrice * 1.1;
+
+
+// const age = parseInt(prompt('Enter your age'));
+// const newAge = age + 1;
+// document.write('Next year you will be ' + newAge);
+
+
+// Number
+// 1, NaN, Infinity
+
+// // String
+// // '', "", ``
+
+// // Boolean
+// // true, false
+
+// // undefined
+// var variable;
+// let anotherVariable;
+// // null
+// variable = null;
+
+// // Object
+// const car = {};
+
+// // Symbol
+// const sym = Symbol();
+
+// const age = parseInt(prompt('Enter your age'));
+
+// if ( (age >= 18) && (age < 70) ) {
+//     document.write('You are adult');
+// } else if (age < 18) {
+//     document.write('You are child');
+// } else {
+//     document.write('You are old');
+// }
+
+// function add(a, b) {
+//     const result = a + b;
+//     return result;
+// }
+
+// const sum = add(100,200.59, 'Hello'); 
+// document.write(sum);
+
+if (0.1 + 0.2 === 0.3) {
+    document.write('TRUE');
+} else {
+    document.write('FALSE');
+}
